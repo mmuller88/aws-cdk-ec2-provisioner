9c7867c0613d184402f854c38742f1ed9251342d
Martin Mueller <damadden88@googlemail.com>
2021-05-12 20:13:04 -0300

    update config.js for prod and dev

--- frontend/src/config/dev/config.js
+++ frontend/src/config/dev/config.js
@@ -1,11 +1,11 @@
 window.ENV = {
     "stage": "dev",
     "aws_project_region": "eu-central-1",
-    "aws_appsync_graphqlEndpoint": "https://msw55sq645esjkf57oouxxnwom.appsync-api.eu-central-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://c7jrrvdunjfcdptlud3aaehi7y.appsync-api.eu-central-1.amazonaws.com/graphql",
     "aws_appsync_region": "eu-central-1",
     "aws_appsync_authenticationType": "AWS_IAM",
     "aws_cognito_region": "eu-central-1",
-    "aws_user_pools_id": "eu-central-1_84KRaoqOo",
-    "aws_user_pools_web_client_id": "4c3407a1lda87eng5t4oselab5",
-    "aws_cognito_identity_pool_id": "eu-central-1:4a7687f1-4138-4212-addc-43573d112483"
+    "aws_user_pools_id": "eu-central-1_vmLR2zHpD",
+    "aws_user_pools_web_client_id": "7c4hhru1h0hoamodel7n8g800l",
+    "aws_cognito_identity_pool_id": "eu-central-1:d6ea3ba8-bef2-44b4-a995-ec17083e4092"
 }
--- frontend/src/config/prod/config.js
+++ frontend/src/config/prod/config.js
@@ -1,11 +1,11 @@
 window.ENV = {
     "stage": "prod",
     "aws_project_region": "eu-central-1",
-    "aws_appsync_graphqlEndpoint": "https://vtaskldpgvak7fzuohxjiig4bq.appsync-api.eu-central-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://ncis4u5vzfghpcd5yy3fwwtam4.appsync-api.eu-central-1.amazonaws.com/graphql",
     "aws_appsync_region": "eu-central-1",
     "aws_appsync_authenticationType": "AWS_IAM",
     "aws_cognito_region": "eu-central-1",
-    "aws_user_pools_id": "eu-central-1_X1i2P00ye",
-    "aws_user_pools_web_client_id": "ulmks6a0j0k65jfipgej4vpao",
-    "aws_cognito_identity_pool_id": "eu-central-1:5357c562-4eb9-4013-8e8a-68fc1570f812"
+    "aws_user_pools_id": "eu-central-1_mHGwQzcgi",
+    "aws_user_pools_web_client_id": "1fvv8nug42jt1c7rl1biredjr3",
+    "aws_cognito_identity_pool_id": "eu-central-1:cf006e98-6a9d-48dd-8faf-e4f24f1feda6"
 }
