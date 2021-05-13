9cb94ad24dd4387d8298bda843a225e6d5011063
Martin Mueller <damadden88@googlemail.com>
2021-05-13 06:46:12 -0300

    add posts component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 import { API } from './lib/fetcher';
 
-// import { Posts } from './components/todos';
+import { Posts } from './components/posts';
 import { Todos } from './components/todos';
 
 import Amplify from 'aws-amplify';
@@ -27,17 +27,18 @@ function App() {
   return (
     <div className="App">
       <nav className="Navbar">
-        <h1 className="navbar-logo">AppSync Transformer Demo - {window.ENV.stage || 'no'} Stage</h1>
+        <h1 className="navbar-logo">AppSync Transformer Demo</h1>
         <ul className="nav-menu">
+          <li> <a href="/posts">Posts</a></li>
           <li> <a href="/todos">Todos</a></li>
         </ul>
-        <AmplifySignOut />
       </nav>
       <div>
         <Router>
           <Switch>
-            <Route exact path="/" render={(props: any) => <Todos {...props} />} />
-            <Route path="/todos" render={(props: any) => <Todos {...props} />} />
+            <Route exact path="/" render={(props: any) => <Posts {...props}  />} />
+            <Route path="/posts" render={(props: any) => <Posts {...props}  />} />
+            <Route path="/todos" render={(props: any) => <Todos {...props}  />} />
           </Switch>
         </Router>
       </div>
--- frontend/src/components/todos.tsx
+++ frontend/src/components/todos.tsx
@@ -1,77 +1,30 @@
-import { useState } from 'react';
-import { Auth } from '@aws-amplify/auth';
-import { useMutation } from 'react-query';
+import React from 'react';
 
-import { TodoItem, useTodoListQuery, TodoAddDocument } from '../lib/api';
-import { API } from '../lib/fetcher';
-
-const initialState = { body: '', username: ''  };
-declare const window: any;
+import { useListTodosQuery } from '../lib/api';
 
 export function Todos() {
-  const [todo, setTodo] = useState(initialState);
-  const { body } = todo;
-
-  const { data, isLoading, refetch } = useTodoListQuery(null, {
+  const { data, isLoading } = useListTodosQuery(null, {
     refetchOnWindowFocus: false
   });
-
-  const [createTodo] = useMutation(async (input: any) => {
-    const result = await API.getInstance().query(TodoAddDocument, { ...input });
-    console.log(result);
-    return result.data?.todoAdd as TodoItem;
-  });
-
-  const onChange = (e) => {
-    setTodo(() => ({ ...todo, [e.target.name]: e.target.value }))
-  }
-
-  const createNewTodo = async () => {
-    if (!body) return
-
-    const userData = await Auth.currentAuthenticatedUser();
-
-    const input = {
-      ...todo,
-      username: userData.username
-    };
-
-    const createResult = await createTodo(input);
-    if (createResult) {
-      refetch();
-    }
-  }
 
   if (isLoading) return <div>Loading...</div>;
 
   return (
-    <div>
+    <div className="App">
       <div>
         <h2>Todos:</h2>
         {
-          data?.todoList
-            ? data?.todoList?.map(todo => {
+          data?.listTodos.length > 0
+            ? data?.listTodos.map(todo => {
               return (
                 <div>
-                  <h4>Body: {todo.body}</h4>
-                  {window.ENV.stage === 'dev'? <h5>Id: {todo.id}</h5> : ''}
-                  <h5>Username: {todo.username}</h5>
+                  <h4>Title: {todo.title}</h4>
+                  <h5>Owner: {todo.userId}</h5>
                 </div>
               )
             })
             : <h4>No todos found</h4>
         }
-      </div>
-      <br />
-      <br />
-      <div>
-        <h3>Create Todo:</h3>
-        <div>
-          <textarea onChange={onChange} name="body" placeholder="Body" />
-        </div>
-        <div>
-          <button onClick={createNewTodo}>Create Todo</button>
-        </div>
       </div>
     </div>
   );
