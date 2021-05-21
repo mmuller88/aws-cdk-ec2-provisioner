a19f49c14b85d2fa77ab2a616d9faeebf2ceac5e
Martin Mueller <damadden88@googlemail.com>
2021-05-21 08:36:33 -0300

    persist userPool

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,11 +1,11 @@
 window.ENV = {
     "stage": "dev",
     "aws_project_region": "eu-central-1",
-    "aws_appsync_graphqlEndpoint": "https://c7jrrvdunjfcdptlud3aaehi7y.appsync-api.eu-central-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://qxilmjwslbg6lcqw34bw3vygi4.appsync-api.eu-central-1.amazonaws.com/graphql",
     "aws_appsync_region": "eu-central-1",
     "aws_appsync_authenticationType": "AWS_IAM",
     "aws_cognito_region": "eu-central-1",
-    "aws_user_pools_id": "eu-central-1_vmLR2zHpD",
-    "aws_user_pools_web_client_id": "7c4hhru1h0hoamodel7n8g800l",
-    "aws_cognito_identity_pool_id": "eu-central-1:d6ea3ba8-bef2-44b4-a995-ec17083e4092"
+    "aws_user_pools_id": "eu-central-1_SSeYXA58i",
+    "aws_user_pools_web_client_id": "3cm9cpuc8pncckrjdi83qt5jg9",
+    "aws_cognito_identity_pool_id": "eu-central-1:11bdef90-8187-48d4-94bc-6f9d2d505694"
 }
--- frontend/src/config/prod/config.js
+++ frontend/src/config/prod/config.js
@@ -1,11 +1,11 @@
 window.ENV = {
     "stage": "prod",
     "aws_project_region": "eu-central-1",
-    "aws_appsync_graphqlEndpoint": "https://ncis4u5vzfghpcd5yy3fwwtam4.appsync-api.eu-central-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://y2tphnanvfbnjffnmti6idkucu.appsync-api.eu-central-1.amazonaws.com/graphql",
     "aws_appsync_region": "eu-central-1",
     "aws_appsync_authenticationType": "AWS_IAM",
     "aws_cognito_region": "eu-central-1",
-    "aws_user_pools_id": "eu-central-1_mHGwQzcgi",
-    "aws_user_pools_web_client_id": "1fvv8nug42jt1c7rl1biredjr3",
-    "aws_cognito_identity_pool_id": "eu-central-1:cf006e98-6a9d-48dd-8faf-e4f24f1feda6"
+    "aws_user_pools_id": "eu-central-1_EZGfgkrak",
+    "aws_user_pools_web_client_id": "7n7d0af4v8hf3lhruvugg3hauk",
+    "aws_cognito_identity_pool_id": "eu-central-1:2136ca2d-b55b-4b55-9d3d-907bbea66ace"
 }
