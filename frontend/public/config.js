49a53d0839b7dc2decccdc9e187dfa1aad12cac9
Martin Mueller <damadden88@googlemail.com>
2021-05-23 18:48:24 -0300

    simplify chat

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,11 +1,11 @@
 window.ENV = {
     "stage": "dev",
     "aws_project_region": "eu-central-1",
-    "aws_appsync_graphqlEndpoint": "https://qyuuxgb3hngkjitf4254hixopy.appsync-api.eu-central-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://hhdt4rtj7jbizfgrp37teqrn2m.appsync-api.eu-central-1.amazonaws.com/graphql",
     "aws_appsync_region": "eu-central-1",
     "aws_appsync_authenticationType": "AWS_IAM",
     "aws_cognito_region": "eu-central-1",
     "aws_user_pools_id": "eu-central-1_SSeYXA58i",
-    "aws_user_pools_web_client_id": "6jko5soqqo7lrqelrkrp59n7dm",
-    "aws_cognito_identity_pool_id": "eu-central-1:aa8922dc-79bf-4b5c-b415-9caed23cf3ba"
+    "aws_user_pools_web_client_id": "6l9ha4brh7dioi9eojksb5tdiq",
+    "aws_cognito_identity_pool_id": "eu-central-1:f24c1adf-3ae5-4c70-9d64-00d46ca52bfa"
 }
