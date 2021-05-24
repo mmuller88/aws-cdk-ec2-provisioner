c4b868b2fa998fb5337c83fbcb50557a721584c0
Martin Mueller <damadden88@googlemail.com>
2021-05-24 12:30:18 -0300

    simplify chat

--- frontend/src/config/dev/config.js
+++ frontend/src/config/dev/config.js
@@ -1,15 +1,15 @@
 window.ENV = {
     "Auth": {
-        "identityPoolId": "eu-central-1:d8a158e1-f174-49da-8137-7d7566f839f9",
+        "identityPoolId": "eu-central-1:58cd6990-7493-4c00-b2bc-a3c5e0fdc337",
         "region": "eu-central-1"
     },
-    "stage": "dev",
     "aws_project_region": "eu-central-1",
-    "aws_appsync_graphqlEndpoint": "https://vzu3kh5mfbh3rhbsugttqaw5uy.appsync-api.eu-central-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://hbnypyxmvnh5pkflbbjiqm5mni.appsync-api.eu-central-1.amazonaws.com/graphql",
     "aws_appsync_region": "eu-central-1",
     "aws_appsync_authenticationType": "AWS_IAM",
     "aws_cognito_region": "eu-central-1",
     "aws_user_pools_id": "eu-central-1_SSeYXA58i",
-    "aws_user_pools_web_client_id": "7a09lr9hqu94ag9hcl9488i67n",
-    "aws_cognito_identity_pool_id": "eu-central-1:d8a158e1-f174-49da-8137-7d7566f839f9"
+    "aws_user_pools_web_client_id": "7l66aqil33ic09jfnbf56frtr4",
+    "aws_cognito_identity_pool_id": "eu-central-1:58cd6990-7493-4c00-b2bc-a3c5e0fdc337",
+    "stage": "dev"
 }
