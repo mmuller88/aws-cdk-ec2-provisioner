4d12256d73e2aaecce5ea93597ca4732c5d7fd48
Martin Mueller <damadden88@googlemail.com>
2021-05-26 15:23:01 -0300

    scheduler lambda

---
+++ src/lambda/scheduler.ts
@@ -0,0 +1,14 @@
+// import * as lambda from 'aws-lambda';
+// import * as AWS from 'aws-sdk';
+
+// const codebuild = new AWS.CodeBuild();
+
+export async function handler(event: any/*: lambda.DynamoDBStreamEvent*/) {
+  // exports.handler = async (event/*: lambda.DynamoDBStreamEvent*/) => {
+  // console.debug(`event: ${JSON.stringify(event)}`);
+
+  return 'donee';
+  // {
+  //   statusCode: 200,
+  // };
+};
