e8080bc953ea787af1216dd228f202b128df7c2d
Martin Mueller <damadden88@googlemail.com>
2021-05-26 19:34:18 -0300

    scheduler lambda

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ COPY src/lambda/scheduler.ts ./
 RUN npm install -g cdk@1.105.0
 
 COPY ./ ./
+# COPY cdk.out/ ./
 # COPY cdk.out/ec2-vm-stack.template.json ./
 # COPY cdk.out/manifest.json ./
 # COPY cdk.out/cross-region-stack-981237193288:us-east-1.template.json ./
--- src/lambda/scheduler.ts
+++ src/lambda/scheduler.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import * as lambda from 'aws-lambda';
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-const { exec } = require('child_process');
+const execSync = require('child_process').execSync;
 
 // import * as AWS from 'aws-sdk';
 
@@ -11,17 +11,25 @@ export async function handler(event: lambda.DynamoDBStreamEvent) {
   // exports.handler = async (event/*: lambda.DynamoDBStreamEvent*/) => {
   console.debug(`event: ${JSON.stringify(event)}`);
 
-  exec("cdk --app . deploy 'ec2-vm-stack' --require-approval never", (error: any, stdout: any, stderr: any) => {
-    if (error) {
-      console.log(`error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-  });
+  let res;
+  try {
+    res = execSync("cdk --app . deploy 'ec2-vm-stack' --require-approval never");
+  } catch (err) {
+    console.debug(`err: ${err}`);
+  }
+  // , (error: any, stdout: any, stderr: any) => {
+  //   if (error) {
+  //     console.debug(`error: ${error.message}`);
+  //     return;
+  //   }
+  //   if (stderr) {
+  //     console.debug(`stderr: ${stderr}`);
+  //     return;
+  //   }
+  //   console.log(`stdout: ${stdout}`);
+  // });
+
+  console.debug(`res: ${JSON.stringify(res)}`);
 
   return 'doneeee';
   // {
