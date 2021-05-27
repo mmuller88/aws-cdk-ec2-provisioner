b9866631bea4d6fefb61c756538f68c34d1dce26
Martin Mueller <damadden88@googlemail.com>
2021-05-27 08:10:40 -0300

    scheduler lambda

--- src/scheduler-stack.ts
+++ src/scheduler-stack.ts
@@ -46,7 +46,7 @@ export class SchedulerStack extends CustomStack {
 
     cdkSchedulerLambda.addToRolePolicy(
       new statement.Cloudformation().allow().toDescribeStacks().toCreateStack().toCreateChangeSet().toExecuteChangeSet().toDescribeChangeSet()
-        .toGetTemplate(),
+        .toGetTemplate().toDeleteChangeSet(),
     );
     cdkSchedulerLambda.addToRolePolicy(
       new statement.Ssm().allow().toGetParameter(),
