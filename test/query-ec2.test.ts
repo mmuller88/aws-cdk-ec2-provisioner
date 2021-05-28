f1b1fe00138c2b3bbe47e7c921f2389fcab76116
Martin Mueller <damadden88@googlemail.com>
2021-05-27 21:18:57 -0300

    add queryec2 lambda

--- test/query-ec2.test.ts
+++ test/query-ec2.test.ts
@@ -1,10 +1,10 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 // import * as lambda from 'aws-lambda';
-//import { ReservationList } from 'aws-sdk/clients/ec2';
+import { DescribeInstancesResult } from 'aws-sdk/clients/ec2';
 import * as AWS from '../__mocks__/aws-sdk';
 import { handler } from '../src/lambda/query-ec2';
 
-// const ec2 = new AWS.EC2();
+const ec2 = new AWS.EC2();
 
 // test('queryStringParameters.projectName is not existing', async () => {
 //   const event = {};
@@ -15,22 +15,27 @@ import { handler } from '../src/lambda/query-ec2';
 // });
 
 test('simple listEc2', async () => {
-  const mockResult = [
-    {
-      Instances: [{
-        InstanceId: 'i-123',
-        Tags: [
-          { Key: 'Owner', Value: 'Hacklab' },
-          { Key: 'Name', Value: 'doiiing' },
-        ],
-      }],
-    },
-  ];
+  const mockResult: DescribeInstancesResult = {
+    Reservations: [
+      {
+        Instances: [{
+          InstanceId: 'i-123',
+          Tags: [
+            { Key: 'Owner', Value: 'Hacklab' },
+            { Key: 'Name', Value: 'doiiing' },
+          ],
+          State: {
+            Name: 'running',
+          },
+        }],
+      },
+    ],
+  };
   AWS.describeInstancesResponse.mockReturnValueOnce(mockResult);
 
   const event = { info: { fieldName: 'listEc2' } };
   const response = await handler(event);
-  // expect(codebuild.listBuildsForProject).toHaveBeenCalledWith({ projectName: 'albern' });
+  expect(ec2.describeInstances).toHaveBeenCalled();
 
   expect(response).toEqual([{
     id: 'i-123',
