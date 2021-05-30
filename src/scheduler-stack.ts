1bfd42a027cf1c6ee65cb245570f0612895024fb
Martin Mueller <damadden88@googlemail.com>
2021-05-30 08:51:33 -0300

    save keypair for instance

--- src/scheduler-stack.ts
+++ src/scheduler-stack.ts
@@ -71,7 +71,7 @@ export class SchedulerStack extends CustomStack {
     );
     cdkSchedulerLambda.addToRolePolicy(
       new statement.Iam().allow().toPassRole().toCreateRole().toCreateInstanceProfile().toPutRolePolicy().toAddRoleToInstanceProfile()
-        .toDeleteRolePolicy().toRemoveRoleFromInstanceProfile().toDeleteInstanceProfile().toDeleteRole(),
+        .toDeleteRolePolicy().toRemoveRoleFromInstanceProfile().toDeleteInstanceProfile().toDeleteRole().toGetPolicy(),
     );
     cdkSchedulerLambda.addToRolePolicy(
       new statement.Ec2().allow().toDescribeImages().toCreateSecurityGroup().toDescribeSecurityGroups().toRevokeSecurityGroupEgress().toCreateTags()
