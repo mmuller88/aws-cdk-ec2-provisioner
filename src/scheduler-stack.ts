867ecd9baa321786bc8c70a906692ba75b229ad7
Martin Mueller <damadden88@googlemail.com>
2021-06-02 07:27:36 -0300

    cw alarms

--- src/scheduler-stack.ts
+++ src/scheduler-stack.ts
@@ -1,27 +1,27 @@
 import * as path from 'path';
+import * as cloudwatch from '@aws-cdk/aws-cloudwatch';
 import * as ddb from '@aws-cdk/aws-dynamodb';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as lambdajs from '@aws-cdk/aws-lambda-nodejs';
 import * as logs from '@aws-cdk/aws-logs';
-import * as core from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core';
 import { CustomStack } from 'aws-cdk-staging-pipeline/lib/custom-stack';
 import { AppSyncTransformer } from 'cdk-appsync-transformer';
 import * as statement from 'cdk-iam-floyd';
 
-
-export interface SchedulerStackProps extends core.StackProps {
+export interface SchedulerStackProps extends cdk.StackProps {
   readonly stage: string;
   readonly appSyncTransformer?: AppSyncTransformer;
 }
 
 export class SchedulerStack extends CustomStack {
-  constructor(scope: core.Construct, id: string, props: SchedulerStackProps) {
+  constructor(scope: cdk.Construct, id: string, props: SchedulerStackProps) {
     super(scope, id, props);
     // const dockerfile = path.join(__dirname, '..');
 
     // const cdkSchedulerLambda = new lambda.DockerImageFunction(this, 'scheduler', {
     const cdkSchedulerLambda = new lambdajs.NodejsFunction(this, 'scheduler', {
-      functionName: 'scheduler',
+      // functionName: 'scheduler',
       entry: `${path.join(__dirname)}/lambda/scheduler.ts`,
       bundling: {
         commandHooks: {
@@ -38,8 +38,36 @@ export class SchedulerStack extends CustomStack {
       },
       logRetention: logs.RetentionDays.ONE_DAY,
       environment: {},
-      timeout: core.Duration.minutes(15),
+      timeout: cdk.Duration.minutes(15),
     });
+
+    // 👇 define a metric for lambda errors
+    const functionErrors = cdkSchedulerLambda.metricErrors({
+      period: cdk.Duration.minutes(1),
+    });
+    // 👇 define a metric for lambda invocations
+    // const functionInvocation = cdkSchedulerLambda.metricInvocations({
+    //   period: cdk.Duration.minutes(1),
+    // });
+
+    // 👇 create an Alarm using the Alarm construct
+    new cloudwatch.Alarm(this, 'lambda-errors-alarm', {
+      metric: functionErrors,
+      threshold: 1,
+      comparisonOperator:
+        cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+      evaluationPeriods: 1,
+      alarmDescription:
+        'Alarm if the SUM of Errors is greater than or equal to the threshold (1) for 1 evaluation period',
+    });
+
+    // // 👇 create an Alarm directly on the Metric
+    // functionInvocation.createAlarm(this, 'lambda-invocation-alarm', {
+    //   threshold: 1,
+    //   evaluationPeriods: 1,
+    //   alarmDescription:
+    //     'Alarm if the SUM of Lambda invocations is greater than or equal to the  threshold (1) for 1 evaluation period',
+    // });
 
     if (props.appSyncTransformer) {
       const streamArn = props.appSyncTransformer.addDynamoDBStream({
