3c65253bce392e0515a54ad3f88c91154b68decb
Martin Mueller <damadden88@googlemail.com>
2021-06-04 22:00:53 -0300

    init slack message

--- src/lambda/slack.ts
+++ src/lambda/slack.ts
@@ -1,3 +1,4 @@
+import * as lambda from 'aws-lambda';
 import * as axios from 'axios';
 
 // curl -X POST --data-urlencode "payload={\"channel\": \"#hacklab\", \"username\": \"webhookbot\", \"text\": \"This is posted to #hacklab and comes from a bot named webhookbot.\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/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
@@ -19,18 +20,25 @@ export interface SlackMessage {
   icon_emoji?: string;
 }
 
-export async function handler(event: any) {
+export async function handler(event: lambda.SNSEvent) {
   console.debug(`event: ${JSON.stringify(event)}`);
 
   const webhook = process.env.SLACK_WEBHOOK || '';
-  const slackMessage: SlackMessage = {
-    text: 'ahoi von dort',
-  };
-  const axiosResult = await axios.default
-    .post(webhook, slackMessage, {
-      headers: {
-        'Content-type': 'application/json',
-      },
-    });
-  return axiosResult.data;
+
+  event.Records.map(async record => {
+    const slackMessage: SlackMessage = {
+      username: 'naala',
+      text: record.Sns.Message,
+    };
+    const axiosResult = await axios.default
+      .post(webhook, slackMessage, {
+        headers: {
+          'Content-type': 'application/json',
+        },
+      });
+    console.debug(`axiosResult: ${JSON.stringify(axiosResult)}`);
+
+  });
+
+  return 'done';
 }
