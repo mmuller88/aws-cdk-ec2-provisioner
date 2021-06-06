55808997a3fad7d07dbe31cd98090b71a9ad27a9
Martin Mueller <damadden88@googlemail.com>
2021-06-06 08:11:50 -0300

    cfn lambda eng

--- frontend/src/config/dev/config.js
+++ frontend/src/config/dev/config.js
@@ -1,11 +1,11 @@
 window.ENV = {
     "aws_project_region": "eu-central-1",
-    "aws_appsync_graphqlEndpoint": "https://if76fu6xorhdridx6g7l4xxqry.appsync-api.eu-central-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://cmbxslxqkvgxxc44pmwsbtokni.appsync-api.eu-central-1.amazonaws.com/graphql",
     "aws_appsync_region": "eu-central-1",
     "aws_appsync_authenticationType": "AWS_IAM",
     "aws_cognito_region": "eu-central-1",
     "aws_user_pools_id": "eu-central-1_SSeYXA58i",
-    "aws_user_pools_web_client_id": "2q6qi2ing4f41i40521f049auh",
-    "aws_cognito_identity_pool_id": "eu-central-1:5548c0de-2899-4f9e-94df-86436da47ad9",
+    "aws_user_pools_web_client_id": "7evqum2ue3i4dbsk3kg3pq4ki8",
+    "aws_cognito_identity_pool_id": "eu-central-1:c7850678-f874-47d5-8c29-6a16f765e683",
     "stage": "dev"
 }
